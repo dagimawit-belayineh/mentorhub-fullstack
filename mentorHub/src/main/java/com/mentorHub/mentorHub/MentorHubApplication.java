6348668311ffbac2a59f8d5167d03119dcdfc3f1
dagimawit-belayineh <dagimawitbelayineh@gmail.com>
2025-10-23 13:32:43 +0300

    feat: initial project setup

---
+++ mentorHub/src/main/java/com/mentorHub/mentorHub/MentorHubApplication.java
@@ -0,0 +1,13 @@
+package com.mentorHub.mentorHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorHubApplication.class, args);
+	}
+
+}
---
+++ mentorHub/src/test/java/com/mentorHub/mentorHub/MentorHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentorHub.mentorHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
