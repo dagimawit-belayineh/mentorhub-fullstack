c0a14df3c51334ee314be445dbbab5c370560c8f
dagimawit-belayineh <dagimawitbelayineh@gmail.com>
2025-11-03 19:44:22 +0300

    user service

--- mentorHub/src/main/java/com/mentorHub/mentorHub/payment/dto/UserDto.java
+++ mentorHub/src/main/java/com/mentorHub/mentorHub/payment/dto/UserDto.java
@@ -1,4 +1,31 @@
 package com.mentorHub.mentorHub.payment.dto;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class UserDto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    @NotNull(message= "first name can not be null")
+    @Size(min = 5 , max = 50)
+    private String firstName;
+    @NotNull(message= "last name can not be null")
+    @Size(min = 5 , max = 50)
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    @NotNull(message= "fayda number must not be null")
+    private String faydaNumber;
+    private boolean verified  = false;
+    private String role;
+
 }
--- mentorHub/src/main/java/com/mentorHub/mentorHub/payment/service/UserService.java
+++ mentorHub/src/main/java/com/mentorHub/mentorHub/payment/service/UserService.java
@@ -1,4 +1,33 @@
 package com.mentorHub.mentorHub.payment.service;
 
+import com.mentorHub.mentorHub.auth.controller.service.FaydaAuthService;
+import com.mentorHub.mentorHub.payment.dto.UserDto;
+import com.mentorHub.mentorHub.payment.model.User;
+import com.mentorHub.mentorHub.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
-}
+    private UserRepository userRepository;
+    private FaydaAuthService authService;
+    private BCryptPasswordEncoder passwordEncode;
+ public UserService(UserRepository userRepository, FaydaAuthService authService) {
+     this.userRepository = userRepository;
+     this.authService = authService;
+ }
+ public User registerUser(UserDto userDto){
+     if (userRepository.findByEmail(userDto.getEmail()).isPresent()){
+         throw new RuntimeException("Email already exists");
+
+     }
+     User user = new User();
+     user.setFirstName(userDto.getFirstName());
+     user.setLastName(userDto.getLastName());
+     user.setEmail(userDto.getEmail());
+     user.setFaydaNumber(userDto.getFaydaNumber());
+     user.setRole(userDto.getRole() != null ? userDto.getRole() : "USER");
+     return userRepository.save(user);
+ }
+ }
+
